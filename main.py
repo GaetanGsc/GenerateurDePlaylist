661e5841ed3387ede6409431379b780cbf884c77
Gaetan GASCHE <gaetan.gasche@bts-malraux.net>
2014-10-07 15:56:01 +0200

    Modification du main du 01/10/14

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+#!/usr/bin/python3
+
+# Fonction pour controler si les arguments sont correctement renseigner
+def verifArguments(liste_arg):
+    logging.info("verifie argument 2")
+    try:
+        nb = int(liste_arg[1])
+        if nb<0:
+            nb = abs(nb)
+            logging.warning('La quantité saisie doit etre positive')
+            logging.info('Nombre négatif transformé en positif: ' + str(nb))
+        elif nb>100:
+            nb = None
+            logging.warning('La quantité saisie est supérieur à 100')
+            logging.info('Nombre supérieur à 100 transformé en : ' + str(nb))
+
+        #setattr(liste_arg,nomArg, [liste_arg[0],nb])
+        return True
+    except ValueError:
+        print ("Impossible de convertir \"" + liste_arg[1] + "\" en nombre entier !")
+        logging.error('Impossible de convertir ' + liste_arg[1] + ' en nombre entier !')
+        logging.debug(' *****************************************')
+        exit(1)
+
+# Début du main
 import argparse
 import logging
 
 logging.basicConfig(filename='fichier_de.log', level=logging.DEBUG)
 
 parser = argparse.ArgumentParser()
-    
+
 # Arguments obligatoire
 parser.add_argument("temps", type=int, help="informer la duree en minutes")
 parser.add_argument("nom", help="indiquer le nom du fichier de sortie")
@@ -23,30 +48,16 @@ logging.info(args.temps)
 logging.info(args.nom)
 logging.info(args.format)
 
-# Fonction pour controler si les arguments sont correctement renseigner 
-def verifArguments(arg, nomArg):
-    try:
-        int(arg[1])
-        if int(arg[1]) > 0 and int(arg[1]) <= 100: 
-            logging.info(str(int(arg[1])))
-            setattr(agrs,nomArg, [arg[0],nb])
-            return True
-            logging.error('Veuillez recommencer')
-    except ValueError:
-        logging.error('Veuillez saisir un entier')
 
-if args.genre:
-    logging.info('Genre : ' +args.genre[0])
-    logging.info('Temps : ' +args.genre[1])
+# On affiche les arguments optionnel s'ils sont présent
+for ARG in ['genre','artiste','album','titre']:
+    if getattr(args, ARG) is not None:
+        logging.info(' Argument --' + ARG + ' :\t' + getattr(args, ARG)[0] + '  ' + getattr(args, ARG)[1])
+# On vérifie que le 2em sous argument de genre est bien un entier naturel
+if verifArguments(args.genre) == True:
+    print ('ok')
+    logging.debug(' *****************************************')
 
-if args.artiste:
-    logging.info('Artiste : ' +args.artiste[0])
-    logging.info('Temps : ' +args.artiste[1])
-
-if args.album:
-    logging.info('Album : ' +args.album[0])
-    logging.info('Temps : ' +args.album[1])
-    
 #variable de poucentage
 
 #pourcentage = args.temps-(1-args.genre[1]/100)
